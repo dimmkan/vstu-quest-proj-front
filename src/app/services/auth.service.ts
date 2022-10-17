95e52d234a381f600b263ad155a34dcd033d1295
Dmitry Kanaev <dimmkan@mail.ru>
2022-10-17 12:37:54 +0300

    Fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -24,33 +24,37 @@ export class AuthService {
   }
 
   login() {
-    this.http.post(`${environment.api_url}/auth/login`, {
-      login: this.userLogin,
-      password: this.userPw
-    })
-      .subscribe((response: any) => {
-        if (response.access_token) {
-          const token_chunks = response.access_token.split('.');
-          const paramsObj = JSON.parse(atob(token_chunks[1]));        
-          //@ts-ignore
-          this._isAuth = paramsObj.isAuth
-          if (this._isAuth) {
-            let date = new Date();
-            date.setHours(date.getHours() + 2)
-            this.cookie.set('isAuth', 'true', { expires: date })
+      this.http.post(`${environment.api_url}/auth/login`, {
+        login: this.userLogin,
+        password: this.userPw
+      })
+        .subscribe((response: any) => {
+          if (response.access_token) {
+            const token_chunks = response.access_token.split('.');
+            const paramsObj = JSON.parse(atob(token_chunks[1]));        
             //@ts-ignore
-            this.cookie.set('roles', paramsObj.roles, { expires: date })
-            this.cookie.set('username', this.userLogin, { expires: date })
-            //@ts-ignore
-            this.cookie.set('userId', paramsObj.userId, { expires: date })
+            this._isAuth = paramsObj.isAuth
+            if (this._isAuth) {
+              let date = new Date();
+              date.setHours(date.getHours() + 2)
+              this.cookie.set('isAuth', 'true', { expires: date })
+              //@ts-ignore
+              this.cookie.set('roles', paramsObj.roles, { expires: date })
+              this.cookie.set('username', this.userLogin, { expires: date })
+              //@ts-ignore
+              this.cookie.set('userId', paramsObj.userId, { expires: date })
+            } else {
+              this.isError = true
+            }
           } else {
             this.isError = true
           }
-        } else {
-          this.isError = true
-        }
-      })
-
+        }, (error) => {
+          if (error) {
+            this.isError = true
+            console.log(error);
+          }
+        })
   }
 
   logout() {
